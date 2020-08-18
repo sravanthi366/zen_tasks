8cf66b7d3997e6a6067345a1e1b761940cc3a910
sravanthi366 <sravanthimca95@gmail.com>
2020-08-18 13:19:11 +0530

    task submition

---
+++ week5/Day-1 -17-08-20/app.js
@@ -0,0 +1,47 @@
+setTimeout(function() {
+    document.getElementById('section')
+    section.innerHTML = 10
+}, 1000);
+setTimeout(function() {
+    document.getElementById('section')
+    section.innerHTML = 9
+}, 2000);
+setTimeout(function() {
+    document.getElementById('section')
+    section.innerHTML = 8
+}, 3000);
+setTimeout(function() {
+    document.getElementById('section')
+    section.innerHTML = 7
+}, 4000);
+setTimeout(function() {
+    document.getElementById('section')
+    section.innerHTML = 6
+}, 5000);
+setTimeout(function() {
+    document.getElementById('section')
+    section.innerHTML = 5
+}, 6000);
+setTimeout(function() {
+    document.getElementById('section')
+    section.innerHTML = 4
+}, 7000);
+setTimeout(function() {
+    document.getElementById('section')
+    section.innerHTML = 3
+}, 8000);
+setTimeout(function() {
+    document.getElementById('section')
+    section.innerHTML = 2
+}, 9000);
+setTimeout(function() {
+    document.getElementById('section')
+    section.innerHTML = 1
+}, 10000);
+
+
+setTimeout(function() {
+
+    document.getElementById('section')
+    section.innerHTML = '<h2>Happy Independence Day!!!</h2>'
+}, 11000);
